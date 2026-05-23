82f0ce50c329b93b68d67237dc3987d699b67834
Mohamed Adnane LARHRISSI <mohamedadnanelarhrissi@gmail.com>
2026-05-24 00:54:19 +0200

    feat(ota): implement ota_receive_packet (SOF resync + CRC16 check)

--- bootloader/Core/Src/ota_proto.c
+++ bootloader/Core/Src/ota_proto.c
@@ -25,9 +25,42 @@ void ota_send_nak(void) {
 }
 
 bool ota_receive_packet(ota_packet_t *out) {
-    /* À implémenter */
-    (void)out;
-    return false;
+    uint8_t sof; 
+    do {
+       if (HAL_UART_Receive(&huart2, &sof, 1, 5000) != HAL_OK){
+            return false;
+        }
+    }while(sof != OTA_SOF);
+
+    if (HAL_UART_Receive(&huart2, &out->seq, 1, 1000) != HAL_OK){
+        return false;
+    }
+    if (HAL_UART_Receive(&huart2, (uint8_t*)&out->len, 2, 1000) != HAL_OK){
+        return false;
+    }
+
+    if (out->len > sizeof(out->data)) return false;
+
+    if (HAL_UART_Receive(&huart2, out->data, out->len, 2000) != HAL_OK){
+        return false;
+    }
+
+    uint16_t crc_recu;
+    if (HAL_UART_Receive(&huart2, (uint8_t*)&crc_recu, 2, 1000) != HAL_OK) return false;
+
+    /* Recalculer le CRC16 sur SEQ + LEN + DATA */
+    uint8_t crc_buf[3 + sizeof(out->data)];
+    crc_buf[0] = out->seq;
+    crc_buf[1] = (uint8_t)(out->len & 0xFF);          // LEN low
+    crc_buf[2] = (uint8_t)((out->len >> 8) & 0xFF);   // LEN high
+    for (uint16_t i = 0; i < out->len; i++) {
+            crc_buf[3 + i] = out->data[i];
+    }
+
+    uint16_t crc_calc = crc16_calc(crc_buf, 3 + out->len);
+
+    return (crc_calc == crc_recu);
+    
 }
 
 void ota_run_session(void) {
